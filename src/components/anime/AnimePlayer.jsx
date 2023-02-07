1a33ec98de3117eef54c76120c8743be8ef968b2
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2023-02-08 01:07:46 +0530

    update

--- src/components/anime/AnimePlayer.jsx
+++ src/components/anime/AnimePlayer.jsx
@@ -5,7 +5,7 @@ const AnimePlayer = ({ img, title, episodeList }) => {
   const [value, setValue] = useState(1);
   const [epUrl, setEpUrl] = useState([]);
   const [loading, setLoading] = useState(true);
-
+  const [toggle, setToggle] = useState(true);
   const animeEpUrl = import.meta.env.VITE_ANIME_DETAILS;
 
   useEffect(() => {
@@ -17,34 +17,47 @@ const AnimePlayer = ({ img, title, episodeList }) => {
         const info = await response.json();
         setEpUrl(info);
         setLoading(false);
+        console.log( info)
       } catch (error) {
         console.log(error);
       }
     };
     fetchInfo();
-  }, []);
+  }, [value]);
 
   const { sources_bk } = epUrl;
 
-  if (loading) {
-    return <Loading />;
-  }
+  // if (loading) {
+  //   return <Loading />;
+  // }
 
   return (
     <>
-<h1 className='text-lg mb-3 font-medium'>Episode: {value}</h1>
-      <ReactPlayer
-        className='react-player mt-4 mb-4'
-        url={sources_bk[0].file}
-        controls={true}
-        playsinline={true}
-        pip={true}
-        light={img}
-        volume={1}
-        width={"100%"}
-        height={"300px"}
-        stopOnUnmount={false}
-      />
+      <h1 className='text-lg mb-3 font-medium'>Episode: {value}</h1>
+
+      {loading ? (
+      <div className="stats shadow flex justify-center">
+  
+      <div className="stat ">
+        <div className="stat-value  ">Loading....</div>
+      </div>
+      
+    </div>
+      ) : (
+        <ReactPlayer
+          className='react-player mt-4 mb-4'
+          url={sources_bk[0].file}
+          label={sources_bk[0].label}
+          controls={true}
+          playsinline={true}
+          pip={true}
+          light={img}
+          volume={1}
+          width={"100%"}
+          height={"300px"}
+          stopOnUnmount={false}
+        />
+      )}
 
       <div className='carousel rounded-box card-body '>
         <div className='grid grid-cols-3'>
@@ -52,7 +65,11 @@ const AnimePlayer = ({ img, title, episodeList }) => {
             <button
               key={ep.episodeId}
               className='btn btn-xs gap-1 m-2 text-sm text'
-              onClick={() => setValue(ep.episodeNum)}
+              onClick={() => {
+                setValue(ep.episodeNum);
+                setLoading(true);
+                setEpUrl([]);
+              }}
             >
               episode-{ep.episodeNum}
             </button>
